2a58fbe52076d6af2ebe17bad1774ef5591e661d
asdfg6548 <asdfg6548@syuin.ac.kr>
2024-04-19 10:37:19 +0900

    Initial commit(AJAX)

---
+++ src/main/java/ac/su/ajaxspringinclass/AjaxSpringinClassApplication.java
@@ -0,0 +1,13 @@
+package ac.su.ajaxspringinclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjaxSpringinClassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AjaxSpringinClassApplication.class, args);
+    }
+
+}
